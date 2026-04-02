327c13cceb3b06b4f266e62ba89d478fc0d03580
Camarederic <camarederic@gmail.com>
2026-04-02 20:21:00 +0300

    added styles for the landing section and code for the swiper CSS & JS

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
-'use strict'
+"use strict";
 
+const swiper = new Swiper(".swiper", {
+  autoplay: {
+    delay: 4000,
+    disableOnInteraction: false,
+  },
+  effect: "fade",
+  loop: true,
+
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
